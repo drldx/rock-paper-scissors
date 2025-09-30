03bbf4a9b07a24f6ce34bf9c7358e34c5b85650f
drldx <168914904+drldx@users.noreply.github.com>
2025-09-30 14:43:30 +0530

    feat: function to get computer input
    Declared a function to randomly chose an input for the computer
    Using floor and random methods of Math , created a function which returns either of the three values;

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log("hello world!");
+function getComputerChoice() {
+  
+  let random = Math.floor(Math.random() * 10);
+  let choice; 
+  if (random >=0 && random <= 3) {
+    choice = "Rock"; 
+  } else if (random >=4 && random <=6) {
+    choice = "Paper";
+  } else if (random >= 7 && random <=9) {
+    choice = "Scissors";
+  }
+  
+  return choice;
+}
+console.log(getComputerChoice());
+
 
