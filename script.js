1e68328a0dbcd94c6c5d08636992d54c15d4c717
drldx <168914904+drldx@users.noreply.github.com>
2025-09-30 15:44:52 +0530

    feat: implemented function playRound
    Function to compare the values of userchoice and computerchoice
    and to return the result of the round;

--- script.js
+++ script.js
@@ -16,12 +16,50 @@ function getComputerChoice() {
   
   return choice;
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice() {
   let choice = prompt("Input your choice: ", '').toLowerCase();
   let userChoice = (choice[0].toUpperCase() + choice.slice(1));
   return userChoice;
 }
-console.log(getHumanChoice());
 
+function playRound(humanChoice, computerChoice) {
+  const hC = humanChoice;
+  const cC = computerChoice;
+
+ // console.log(`hc: ${hC}, cc: ${cC}`);
+
+  if (hC === cC) {
+    return "It's a tie!"; 
+  } else if ((hC === "Rock" && cC === "Paper")) {
+    computerScore++;
+    return `You lose! ${cC} beats ${hC}`; 
+
+  } else  if ((hC === "Rock" && cC === "Scissors")) {
+    humanScore++;
+    return `You won! ${hC} beats ${cC}`; 
+
+  } else if ((hC === "Paper" && cC === "Rock")) {
+    humanScore++;
+    return `You won! ${hC} beats ${cC}`; 
+    
+  } else if ((hC === "Paper" && cC === "Scissors")) {
+    computerScore++;
+    return  `You lose! ${cC} beats ${hC}`;
+
+  }else if ((hC === "Scissors" && cC === "Rock")) {
+    computerScore++;
+    return  `You lose! ${cC} beats ${hC}`;
+
+  } else if ((hC === "Scissors" && cC === "Paper")) {
+    humanScore++;
+    return  `You won! ${cC} beats ${hC}`;
+
+  } 
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
